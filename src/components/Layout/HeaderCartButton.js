d6c94ac160064af2a701c013b2e352652c5d1efc
Grigorios Kaframanis <gkaframanis@gmail.com>
2022-02-07 10:54:31 +0200

    Final version of the app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cartContext";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = props => {
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	// Managed by the closest provider which is the CartProvider in App.js
 	const cartCtx = useContext(CartContext);
 
@@ -12,9 +13,28 @@ const HeaderCartButton = props => {
 		return curNumber + item.amount;
 	}, 0);
 
+	const { items } = cartCtx;
+
+	const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+		// After the duration of the animation
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	// The button will be re-evaluated every time the context changes.
 	return (
-		<button className={classes.button} onClick={props.onButtonClick}>
+		<button className={btnClasses} onClick={props.onButtonClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
